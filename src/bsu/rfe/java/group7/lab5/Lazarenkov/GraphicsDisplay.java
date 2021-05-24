e8372980d3f0acbb58e7105f23cb550a65d27fc2
L.A <lazarenkov01@gmail.com>
2021-05-24 20:42:23 +0300

    fixs2

--- src/bsu/rfe/java/group7/lab5/Lazarenkov/GraphicsDisplay.java
+++ src/bsu/rfe/java/group7/lab5/Lazarenkov/GraphicsDisplay.java
@@ -1,4 +1,67 @@
 package bsu.rfe.java.group7.lab5.Lazarenkov;
 
-public class GraphicsDisplay {
-}
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+import javax.swing.JPanel;
+
+public class GraphicsDisplay extends JPanel {
+    private ArrayList<Double[]> graphicsData;
+    private ArrayList<Double[]> originalData;
+    private int selectedMarker = -1;
+    private double minX;
+    private double maxX;
+    private double minY;
+    private double maxY;
+    private double[][] viewport = new double[2][2];
+    private ArrayList<double[][]> undoHistory = new ArrayList(5);
+    private double scaleX;
+    private double scaleY;
+    private BasicStroke axisStroke;
+    private BasicStroke gridStroke;
+    private BasicStroke markerStroke;
+    private BasicStroke selectionStroke;
+    private Font axisFont;
+    private Font labelsFont;
+    private static DecimalFormat formatter = (DecimalFormat)NumberFormat.getInstance();
+    private boolean scaleMode = false;
+    private boolean changeMode = false;
+    private double[] originalPoint = new double[2];
+    private java.awt.geom.Rectangle2D.Double selectionRect = new java.awt.geom.Rectangle2D.Double();
+
+    public GraphicsDisplay() {
+        this.setBackground(Color.WHITE);
+        this.axisStroke = new BasicStroke(2.0F, 0, 0, 10.0F, (float[])null, 0.0F);
+        this.gridStroke = new BasicStroke(1.0F, 0, 0, 10.0F, new float[]{4.0F, 4.0F}, 0.0F);
+        this.markerStroke = new BasicStroke(1.0F, 0, 0, 10.0F, (float[])null, 0.0F);
+        this.selectionStroke = new BasicStroke(1.0F, 0, 0, 10.0F, new float[]{10.0F, 10.0F}, 0.0F);
+        this.axisFont = new Font("Serif", 1, 36);
+        this.labelsFont = new Font("Serif", 0, 10);
+        formatter.setMaximumFractionDigits(5);
+        this.addMouseListener(new GraphicsDisplay.MouseHandler());
+        this.addMouseMotionListener(new GraphicsDisplay.MouseMotionHandler());
+    }
+
+    public void displayGraphics(ArrayList<Double[]> graphicsData) {
+        this.graphicsData = graphicsData;
+        this.originalData = new ArrayList(graphicsData.size());
+        Iterator var3 = graphicsData.iterator();
+
+        while(var3.hasNext()) {
+            Double[] point = (Double[])var3.next();
+            Double[] newPoint = new Double[]{new Double(point[0]), new Double(point[1])};
+            this.originalData.add(newPoint);
+        }
